a230d16c4abe50b7038d8ba56d9b284a7f280c99
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2023-03-17 18:42:55 -0400

    Exercise Complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter the name of the store"
+store_name = gets.chomp
+store_new = Store.create(name: store_name)
+
+if store_new.invalid?
+  store_new.errors.full_messages.each do |error|
+    puts error
+  end
+  puts store_new.valid?
+end
+
